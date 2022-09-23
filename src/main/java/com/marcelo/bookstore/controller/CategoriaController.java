571b87602fd9942d35aaa33d97755dd2edf0be4f
MarceloFLopez <marceloferreiralopez@gmail.com>
2022-09-22 21:17:11 -0300

    Camada Rest e Teste JSON

--- src/main/java/com/marcelo/bookstore/controller/CategoriaController.java
+++ src/main/java/com/marcelo/bookstore/controller/CategoriaController.java
@@ -1,5 +1,72 @@
 package com.marcelo.bookstore.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.ConstraintViolationException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.marcelo.bookstore.domain.Categoria;
+import com.marcelo.bookstore.repository.CategoriaRepository;
+
+@RestController
+@RequestMapping(value = "/categorias")
 public class CategoriaController {
 
+	@Autowired
+	private CategoriaRepository catRepository;
+
+	@RequestMapping("/listar")
+	public List<Categoria> listAll() {
+		List<Categoria> categorias = catRepository.findAll();
+		return categorias;
+	}
+
+	@GetMapping("/{id}")
+	public Optional<Categoria> findById(@PathVariable Integer id) {
+		Optional<Categoria> categoria = catRepository.findById(id);
+		return categoria;
+	}
+
+	@PostMapping
+	public Categoria save(@RequestBody Categoria categoria) {
+		Categoria cadastroSalvar = catRepository.save(categoria);
+		return cadastroSalvar;
+	}
+
+	@DeleteMapping("delete/{id}")
+	public Optional<Categoria> delete(@PathVariable Integer id) {
+		Optional<Categoria> categoria = null;
+		try {
+			categoria = catRepository.findById(id);
+			catRepository.delete(categoria.get());
+		} catch (ConstraintViolationException e) {
+			System.out.println("Erro: " + e.getMessage());
+		} catch (DataIntegrityViolationException e) {
+			System.out.println("Erro: " + e.getMessage());
+		}
+		return categoria;
+	}
+
+	@PutMapping
+	public Categoria update(@RequestBody Categoria categoria) {
+		try {
+			catRepository.save(categoria);
+		} catch (ConstraintViolationException e) {
+			System.out.println("Erro: " + e.getMessage());
+		} catch (DataIntegrityViolationException e) {
+			System.out.println("Erro: " + e.getMessage());
+		}
+		return categoria;
+	}
 }
--- src/main/java/com/marcelo/bookstore/controller/LivroController.java
+++ src/main/java/com/marcelo/bookstore/controller/LivroController.java
@@ -1,5 +1,72 @@
 package com.marcelo.bookstore.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.ConstraintViolationException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.marcelo.bookstore.domain.Livro;
+import com.marcelo.bookstore.repository.LivroRepository;
+
+@RestController
+@RequestMapping(value = "/livros")
 public class LivroController {
 
+	@Autowired
+	private LivroRepository livroRepository;
+
+	@RequestMapping("/listar")
+	public List<Livro> listAll() {
+		List<Livro> livros = livroRepository.findAll();
+		return livros;
+	}
+
+	@GetMapping("/{id}")
+	public Optional<Livro> findById(@PathVariable Integer id) {
+		Optional<Livro> livro = livroRepository.findById(id);
+		return livro;
+	}
+
+	@PostMapping
+	public Livro save(@RequestBody Livro livro) {
+		Livro cadastroSalvar = livroRepository.save(livro);
+		return cadastroSalvar;
+	}
+
+	@DeleteMapping("delete/{id}")
+	public Optional<Livro> delete(@PathVariable Integer id) {
+		Optional<Livro> livro = null;
+		try {
+			livro = livroRepository.findById(id);
+			livroRepository.delete(livro.get());
+		} catch (ConstraintViolationException e) {
+			System.out.println("Erro: " + e.getMessage());
+		} catch (DataIntegrityViolationException e) {
+			System.out.println("Erro: " + e.getMessage());
+		}
+		return livro;
+	}
+
+	@PutMapping
+	public Livro update(@RequestBody Livro livro) {
+		try {
+			livroRepository.save(livro);
+		} catch (ConstraintViolationException e) {
+			System.out.println("Erro: " + e.getMessage());
+		} catch (DataIntegrityViolationException e) {
+			System.out.println("Erro: " + e.getMessage());
+		}
+		return livro;
+	}
 }
