6095023390dc5f2179081b432d91f261fb5c9957
MarceloFLopez <marceloferreiralopez@gmail.com>
2022-09-23 08:57:03 -0300

    Delete para categoria

--- src/main/java/com/marcelo/bookstore/service/CategoriaService.java
+++ src/main/java/com/marcelo/bookstore/service/CategoriaService.java
@@ -37,4 +37,11 @@ public class CategoriaService {
 		obj.setDescricao(objDto.getDescricao());
 		return repository.save(obj);
 	}
+	
+	public void delete(Integer id) {
+		findById(id);
+		repository.deleteById(id);
+	}
+
+
 }
